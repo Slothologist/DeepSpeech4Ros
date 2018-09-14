70860684145543e2fb97f193d1005989de3d85f4
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-09-14 03:34:35 +0200

    - added deepspeech.h for deepspeech version 0.1.1 - barebones main.cpp now creates a model - added missing file for bazel/tensorflow/deepspeech build - CMakeLists updated to include deepspeech and tensorflow libraries - improved Readme.md

--- DeepSpeech4Ros/src/main.cpp
+++ DeepSpeech4Ros/src/main.cpp
@@ -4,11 +4,28 @@
 
 // inludes from this project
 #include "../include/utils.h"
+#include "../include/deepspeech.h"
+
+#define N_CEP 26
+#define N_CONTEXT 9
+#define BEAM_WIDTH 500
+#define LM_WEIGHT 1.75f
+#define WORD_COUNT_WEIGHT 1.00f
+#define VALID_WORD_COUNT_WEIGHT 1.00f
 
 
 utils::config* cfg;
 
 int main(int argc, char *argv[]) {
+
+    const char* model_path = "/home/rfeldhans/programming/audio/deepspeech_install/model/models/output_graph.pb";
+    const char* alphabet_path = "/home/rfeldhans/programming/audio/deepspeech_install/model/models/alphabet.txt";
+    const char* lm_path = "/home/rfeldhans/programming/audio/deepspeech_install/model/models/lm.binary";
+    const char* trie_path = "/home/rfeldhans/programming/audio/deepspeech_install/model/models/trie";
+
+    DeepSpeech::Model model = DeepSpeech::Model(model_path, N_CEP, N_CONTEXT, alphabet_path, BEAM_WIDTH);
+    model.enableDecoderWithLM(alphabet_path, lm_path, trie_path, LM_WEIGHT, WORD_COUNT_WEIGHT, VALID_WORD_COUNT_WEIGHT);
+
     // parse config
     cfg = new utils::config();
     read_config(cfg, argv[1]);
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # DeepSpeech4Ros
 Mozilla Deepspeech for Ros via Jackaudio as part of a larger Pipeline
 
+## Requirements
 
+- Jack audio
+
+- ROS
+
+- Deepspeech
+
+Deepspeech is a bit tricky to install, since we need a linkable library and not just a python wheel.
+Therefore we must build and install Deepspeech ourselves, along with its dependencies, tensorflow and bazel.
+
+But worry not friend, a install script is included in this repo (install_deepspeech.sh) to take all the effort out of that. 
+It will create static libraries for deepspeech and a shared library for tensorlfow and copy them to the DeepSpeech4Ros/libs folder, where the CMakeLists.txt will be looking for it.
+
+Also included is a script to install the necessary system dependencies (install_prerequisites.sh), which 
