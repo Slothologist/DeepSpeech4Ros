8eeeb7e749a5e2f34914f8345ab9a9a01309e1f8
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-02-13 14:51:25 +0100

    changed lib install path

--- install_deepspeech.sh
+++ install_deepspeech.sh
@@ -102,15 +102,11 @@ ok
 cd $MAIN/DeepSpeech/native_client
 make deepspeech
 ok
-mkdir -p $DEEPSPEECH_INSTALL_PREFIX/lib/deepspeech
-cp ${MAIN}/tensorflow/bazel-out/k8*/bin/native_client/*.so $DEEPSPEECH_INSTALL_PREFIX/lib/deepspeech/
-ok
-chmod +w $DEEPSPEECH_INSTALL_PREFIX/lib/deepspeech/libdeepspeech.so
+mkdir -p $DEEPSPEECH_INSTALL_PREFIX/lib
+cp ${MAIN}/tensorflow/bazel-out/k8*/bin/native_client/*.so $DEEPSPEECH_INSTALL_PREFIX/lib/
 ok
 mkdir -p $DEEPSPEECH_INSTALL_PREFIX/include/deepspeech
 cp ${MAIN}/DeepSpeech/native_client/deepspeech.h $DEEPSPEECH_INSTALL_PREFIX/include/deepspeech/
-ok
-chmod +w $DEEPSPEECH_INSTALL_PREFIX/include/deepspeech/deepspeech.h
 ok
 
 duration=$((SECONDS-start))
