2366e9a3fc7d909f9cb13c46f7d0f3b2dad5330d
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-04-08 16:52:43 +0200

    now using quit_esiaf function; fixed wrong typ for ros publisher

--- DeepSpeech4Ros/src/main.cpp
+++ DeepSpeech4Ros/src/main.cpp
@@ -41,6 +41,9 @@ ringbuffer::Ringbuffer* ring_buffer;
 ros::NodeHandle* ros_node_handle;
 ros::Publisher speechPublisher;
 
+// esiaf handle
+esiaf_ros::esiaf_handle *eh;
+
 // callback function for when new audio is availble
 boost::function<void(const std::vector<int8_t> &, const esiaf_ros::RecordingTimeStamps &)> simple_esiaf_callback;
 void esiaf_handler(const std::vector<int8_t> &signal, const esiaf_ros::RecordingTimeStamps & timeStamps){ simple_esiaf_callback(signal, timeStamps); };
@@ -80,7 +83,7 @@ void initialize_deepspeech(){
 void initialize_ros(int argc, char * *argv){
     ros::init(argc, argv, cfg.ros_node_name);
     ros_node_handle = new ros::NodeHandle;
-    speechPublisher = ros_node_handle->advertise<esiaf_ros::SpeechHypothesis>(cfg.esiaf_speech_topic, 1000);
+    speechPublisher = ros_node_handle->advertise<esiaf_ros::SpeechInfo>(cfg.esiaf_speech_topic, 1000);
 
     ROS_INFO("ROS initialized...");
 }
@@ -88,7 +91,7 @@ void initialize_ros(int argc, char * *argv){
 void initialize_esiaf(){
 
     ROS_INFO("starting esiaf initialisation...");
-    esiaf_ros::esiaf_handle *eh = esiaf_ros::initialize_esiaf(ros_node_handle, esiaf_ros::NodeDesignation::SpeechRec);
+    eh = esiaf_ros::initialize_esiaf(ros_node_handle, esiaf_ros::NodeDesignation::SpeechRec);
 
     //create format for output topic
     esiaf_ros::EsiafAudioTopicInfo topicInfo;
@@ -106,13 +109,11 @@ void initialize_esiaf(){
     // notify esiaf about the output topic
     ROS_INFO("adding input topic....");
 
-    float scaling_factor = sizeof(int8_t) / sizeof(int16_t);
-
     simple_esiaf_callback = [&](const std::vector<int8_t> &signal,
                                 const esiaf_ros::RecordingTimeStamps &timeStamps){
-
+        ROS_INFO("esiaf callback");
         int16_t* signal_in_16bit = (int16_t*) signal.data();
-        ring_buffer->push(signal_in_16bit, (size_t) scaling_factor * signal.size());
+        ring_buffer->push(signal_in_16bit, signal.size()/(sizeof(int16_t)/sizeof(int8_t)));
         buffer_end_time = timeStamps.finish;
         buffer_start_time = timeStamps.start;
     };
@@ -120,6 +121,7 @@ void initialize_esiaf(){
 
 
     esiaf_vad_callback = [&](){
+        ROS_INFO("vad callback");
 
         size_t deepspeech_buffer_size = ring_buffer->getSize();
         int16_t* deepspeech_buffer = (int16_t*) malloc(deepspeech_buffer_size * sizeof * deepspeech_buffer);
@@ -129,7 +131,7 @@ void initialize_esiaf(){
         // aquire result
         DsSTT::ds_result result = DsSTT::LocalDsSTT(model, (const short*)deepspeech_buffer,
                                                     deepspeech_buffer_size / 2, 16000);
-        free(deepspeech_buffer);
+        //free(deepspeech_buffer);
 
         // print out result
         ROS_INFO("cpu_time_overall=%.05f",
@@ -178,6 +180,7 @@ int main(int argc, char *argv[]) {
 
     // parse config
     read_config(cfg, argv[1]);
+    ring_buffer = new ringbuffer::Ringbuffer(cfg.max_audio_length);
 
     // initialize all the things!
     initialize_deepspeech();
@@ -193,6 +196,7 @@ int main(int argc, char *argv[]) {
 
     // destructing stuff and freeing buffers
     DS_DestroyModel(model);
+    esiaf_ros::quit_esiaf(eh);
 
     return 0;
 }
